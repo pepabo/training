d5649025874a7f4af3039c729fe925bd6227e5c8
pione30 <pione30@pepabo.com>
2021-06-16 22:04:38 +0900

    Add Showcase

--- frontend-training/app_goal/app/javascript/components/index.jsx
+++ frontend-training/app_goal/app/javascript/components/index.jsx
@@ -1,7 +1,66 @@
 import * as React from "react";
 
+const Showcase = (props) => {
+  const nameLengthThreshold = 8;
+
+  return (
+    <>
+      {/* 中括弧の中で props の値が使えます: */}
+      <p>Hi, I'm {props.name}!</p>
+
+      {/* props の値だけでなく、中括弧の中ではあらゆる JavaScript の式が使えます: */}
+      <p>
+        The answer is {props.answer}, so the doubled answer is{" "}
+        {props.answer * 2}.
+      </p>
+
+      {/* 論理積演算子 && を使って、条件によって要素の表示と非表示を切り替えたり: */}
+      {props.isVisible && <p>This is Visible.</p>}
+
+      {/* 三項演算子を使って、条件によって表示する要素を出し分けたりできます: */}
+      {props.name.length >= nameLengthThreshold ? (
+        <p>name.length is equal or longer than {nameLengthThreshold}.</p>
+      ) : (
+        <p>name.length is less than {nameLengthThreshold}.</p>
+      )}
+
+      {/* 配列に対して map を使えば複数の要素を描画できます: */}
+      <ul>
+        {props.items.map((item) => (
+          <li key={item.id}>
+            {item.name} is {item.price} Yen.
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
 const App = () => {
-  return <p>Hi, I'm Pepayama Botaro!</p>;
+  const name = "Pepayama Botaro";
+  const answer = 42;
+  const isVisible = true;
+  const items = [
+    {
+      id: 1,
+      name: "T-shirt",
+      price: 100,
+    },
+    {
+      id: 2,
+      name: "Hoodie",
+      price: 300,
+    },
+  ];
+
+  return (
+    <Showcase
+      name={name}
+      answer={answer}
+      isVisible={isVisible}
+      items={items}
+    ></Showcase>
+  );
 };
 
 export default App;
