8addbfe88c9b24f62d0edda3e844ad5ef3523215
pione30 <pione30@pepabo.com>
2021-06-16 20:30:55 +0900

    Hello React

---
+++ frontend-training/app_goal/app/javascript/components/index.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = () => {
+  return <p>Hi, I'm Pepayama Botaro!</p>;
+};
+
+export default App;
--- frontend-training/app_goal/app/javascript/packs/application.js
+++ frontend-training/app_goal/app/javascript/packs/application.js
@@ -4,12 +4,18 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import ReactDOM from "react-dom"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
 import "bootstrap"
+import App from "components"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById("app"))
+})
