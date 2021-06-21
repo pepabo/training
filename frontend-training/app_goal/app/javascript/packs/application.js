a0cb083f8d8a9178e0d889c2b6f3553db0a9a6c8
pione30 <pione30@pepabo.com>
2021-06-21 14:58:09 +0900

    :workaround: $ is not defined

--- frontend-training/app_goal/app/javascript/packs/application.js
+++ frontend-training/app_goal/app/javascript/packs/application.js
@@ -16,6 +16,14 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+$("#micropost_image").bind("change", function() {
+  var size_in_megabytes = this.files[0].size/1024/1024;
+  if (size_in_megabytes > 5) {
+    alert("Maximum file size is 5MB. Please choose a smaller file.");
+    $("#micropost_image").val("");
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("app")
   if (container) {
