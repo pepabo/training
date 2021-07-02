27c659c3115e9bffb3772cc6203e36bc09c5397a
pione30 <pione30@pepabo.com>
2021-07-02 11:43:05 +0900

    Add frontend

---
+++ frontend/app_goal/app/javascript/packs/application.js
@@ -0,0 +1,32 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import ReactDOM from "react-dom"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "jquery"
+import "bootstrap"
+import App from "components"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+const renderReactDOM = () => {
+  const container = document.getElementById("app")
+  if (container) {
+    ReactDOM.render(<App />, container)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderReactDOM()
+})
+
+document.addEventListener("turbolinks:load", () => {
+  renderReactDOM()
+})
