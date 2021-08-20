436987a54e26d2ff40fbf3c024432c2d0a7f94b7
tokky <otokita@pepabo.com>
2021-08-20 16:18:31 +0900

    正規表現で重複した記述を削除

--- mobile-training/app_goal/microposts_app/lib/validator.dart
+++ mobile-training/app_goal/microposts_app/lib/validator.dart
@@ -1,6 +1,6 @@
 class Validator {
   static final emailFormat = RegExp(
-      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+      r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
 
   static String? emailValidator(String? email) {
     if (email == null) return "メールアドレスを入力してください。";
