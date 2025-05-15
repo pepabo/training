73013ec76b6c52ef7045f52e4701811df72a13a2
yoshikouki <yoshikouki@pepabo.com>
2025-05-15 09:34:10 +0900

    cp yasslab/sample_apps/7_0/ch14/ frontend-training/app_start

---
+++ frontend-training/app_start/Gemfile
@@ -0,0 +1,53 @@
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "3.2.8"
+
+gem "rails",                      "7.1.5.1"
+gem "image_processing",           "1.12.2"
+gem "active_storage_validations", "0.9.8"
+gem "bcrypt",                     "3.1.18"
+gem "faker",                      "2.21.0"
+gem "will_paginate",              "3.3.1"
+gem "bootstrap-will_paginate",    "1.0.0"
+gem "bootstrap-sass",             "3.4.1"
+gem "sassc-rails",                "2.1.2"
+gem "sprockets-rails",            "3.4.2"
+gem "importmap-rails",            "1.1.5"
+gem "turbo-rails",                "1.4.0"
+gem "stimulus-rails",             "1.2.1"
+gem "jbuilder",                   "2.11.5"
+gem "puma",                       "5.6.8"
+gem "bootsnap",                   "1.16.0", require: false
+gem "sqlite3",                    "1.6.1"
+gem "concurrent-ruby", "1.3.4"
+
+group :development, :test do
+  gem 'reline', '0.5.10'
+  gem "debug",   "1.7.1", platforms: %i[ mri mingw x64_mingw ]
+end
+
+group :development do
+  gem "web-console",         "4.2.0"
+  gem "solargraph",          "0.53.4"
+  gem "irb",                 "1.10.0"
+  gem "repl_type_completor", "0.1.2"
+end
+
+group :test do
+  gem "capybara",                 "3.38.0"
+  gem "selenium-webdriver",       "4.8.3"
+  gem "webdrivers",               "5.2.0"
+  gem "rails-controller-testing", "1.0.5"
+  gem "minitest",                 "5.18.0"
+  gem "minitest-reporters",       "1.6.0"
+  gem "guard",                    "2.18.0"
+  gem "guard-minitest",           "2.4.6"
+end
+
+group :production do
+  gem "aws-sdk-s3", "1.114.0", require: false
+end
+
+# Windows ではタイムゾーン情報用の tzinfo-data gem を含める必要があります
+# gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
---
+++ frontend-training/app_start/config/environments/development.rb
@@ -0,0 +1,90 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable server timing
+  config.server_timing = true
+
+  # Add the following line to disable forgery_protection_origin_check
+  config.action_controller.forgery_protection_origin_check = false
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  host = 'example.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+  config.action_mailer.perform_caching = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  pf_domain = ENV['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => "ALLOW-FROM #{pf_domain}"
+  }
+
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow requests from our preview domain.
+  pf_host = "#{ENV['CODESPACE_NAME']}-3000.#{pf_domain}"
+  config.hosts << pf_host
+
+  config.action_cable.allowed_request_origins = ["https://#{pf_host}"]
+end
